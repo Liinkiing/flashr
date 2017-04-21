19b1798a338f98ed4aecf12390d2638bf846788c
Omar Jbara <omar.jbara2@gmail.com>
2017-04-21 09:12:00 -0400

    - Removed escaping when showing flashr message

--- views/flashes.blade.php
+++ views/flashes.blade.php
@@ -6,7 +6,7 @@
 @if($flashr_type && $flashr_message)
 
     <div style="position: relative" class="alert alert-{{ $flashr_type }}" role="alert">
-        {{ $flashr_message }}
+        {!! $flashr_message  !!}
         <span id="close-flash" onclick="var el = this.parentElement; el.remove()"
               style="position: absolute; top: 10px; right: 10px; font-size: 24px; line-height: 10px; cursor: pointer">&times;</span>
     </div>
